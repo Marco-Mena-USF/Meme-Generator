af7308517d0f23cb9a9c85031c269af0a783fe56
Marco-Mena-USF <130415465+Marco-Mena-USF@users.noreply.github.com>
2023-05-17 14:50:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,107 +1,45 @@
-const gameContainer = document.getElementById("game");
+let memeContainer = document.getElementById('memeContainer');
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple"
-];
+function generateMeme() {
+  let imageUrl = document.getElementById('imageUrlInput').value;
+  let topText = document.getElementById('topTextInput').value;
+  let bottomText = document.getElementById('bottomTextInput').value;
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want to research more
-function shuffle(array) {
-  let counter = array.length;
-
-  // While there are elements in the array
-  while (counter > 0) {
-    // Pick a random index
-    let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
-    counter--;
-
-    // And swap the last element with it
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
+  if (imageUrl === '' || topText === '' || bottomText === '') {
+    alert('Please enter the image URL, top text, and bottom text!');
+    return;
   }
 
-  return array;
+  let meme = createMeme(imageUrl, topText, bottomText);
+  memeContainer.appendChild(meme);
 }
 
-let shuffledColors = shuffle(COLORS);
+function createMeme(imageUrl, topText, bottomText) {
+  let meme = document.createElement('div');
+  meme.className = 'meme';
 
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    // create a new div
-    const newDiv = document.createElement("div");
+  let image = document.createElement('img');
+  image.src = imageUrl;
 
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
+  let topTextElement = document.createElement('div');
+  topTextElement.className = 'text top-text';
+  topTextElement.textContent = topText;
 
-    // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
+  let bottomTextElement = document.createElement('div');
+  bottomTextElement.className = 'text bottom-text';
+  bottomTextElement.textContent = bottomText;
 
-    // append the div to the element with an id of game
-    gameContainer.append(newDiv);
-  }
+  let deleteButton = document.createElement('button');
+  deleteButton.className = 'delete-button';
+  deleteButton.textContent = 'x';
+  deleteButton.onclick = function() {
+    memeContainer.removeChild(meme);
+  };
+
+  meme.appendChild(image);
+  meme.appendChild(topTextElement);
+  meme.appendChild(bottomTextElement);
+  meme.appendChild(deleteButton);
+
+  return meme;
 }
-
-// TODO: Implement this function!
-
-// variables
-let firstCard= null;
-let secondCard = null;
-let cardsFlipped = 0;
-let noClicking = false;
-
-function handleCardClick(event) {
-  if (noClicking) return;
-  if (event.target.classList.contains("flipped")) return;
-
-  let currentCard = event.target;
-  currentCard.style.backgroundColor = currentCard.classList[0];
-
-  if (!firstCard || !secondCard) {
-    currentCard.classList.add("flipped");
-    firstCard = firstCard || currentCard;
-    secondCard = currentCard === firstCard ? null : currentCard;}
-
-  if (firstCard && secondCard) 
-  {noClicking = true;
-    let gif1 = firstCard.className;
-    let gif2 = secondCard.className;
-
-    if (gif1 === gif2) {
-      cardsFlipped += 2;
-      firstCard.removeEventListener("click", handleCardClick);
-      secondCard.removeEventListener("click", handleCardClick);
-      firstCard = null;
-      secondCard = null;
-      noClicking = false;
-    } else {setTimeout(function() {
-      firstCard.style.backgroundColor = "";
-      secondCard.style.backgroundColor = "";
-      firstCard.classList.remove("flipped");
-      secondCard.classList.remove("flipped");
-      firstCard = null;
-      secondCard = null;
-      noClicking = false;
-      }, 1000);
-    }
-  }
-}
-
-// when the DOM loads
-createDivsForColors(shuffledColors);
-
